e7cbbf1561819735bdf6fba914d3523c663031a1
maulik.dave <maulikdave2010@gmail.com>
2020-03-16 17:37:44 +0530

    Add project files.

---
+++ FormAuth_2/Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FormAuth_2.Controllers
+{
+    public class UsersController : Controller
+    {
+        //[AllowAnonymous]
+        [HttpGet]
+        public ActionResult login()
+        {
+            return View();
+        }
+        // GET: Users
+
+        //[AllowAnonymous]
+        [HttpPost]
+        public ActionResult login(User model)
+        {
+            using (MVC_DBEntities context = new MVC_DBEntities())
+            {
+                var isValid = context.Users.Any(x=>x.UserName == model.UserName && x.UserPassword==x.UserPassword);
+                if(isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName,false);
+                    return RedirectToAction("Index","Employees");
+                }
+                ModelState.AddModelError("", "Invalid Cradentials");
+                return View();
+            }
+        }
+
+
+        //[AllowAnonymous]
+        [HttpGet]
+        public ActionResult signup()
+        {
+            return View();
+        }
+        //[AllowAnonymous]
+        [HttpPost]
+        public ActionResult signup(User model)
+        {
+            using (MVC_DBEntities context = new MVC_DBEntities())
+            {
+                if(ModelState.IsValid)
+                {
+                    context.Users.Add(model);
+                    context.SaveChanges();
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    ModelState.AddModelError("","Invalid entry");
+                    return View();
+                }
+               
+            }
+        }
+
+        public ActionResult logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("login");
+        }
+    }
+}
---
+++ FormAuth_2/WebRoleProvider.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace FormAuth_2
+{
+    public class WebRoleProvider : RoleProvider
+    {
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+           
+            using (MVC_DBEntities context = new MVC_DBEntities())
+            {
+                var userID = context.Users.Where(x => x.UserName == username).FirstOrDefault().ID;
+                var UserRoleMap = context.UserRolesMappings.Where(x => x.UserID == userID).ToList();
+                String[] s = new String[UserRoleMap.Count];
+                int i = 0;
+                foreach(UserRolesMapping a in UserRoleMap)
+                {
+                  s[i]=  context.RoleMasters.Where(x => x.ID == a.RoleID).SingleOrDefault().RollName;
+                    i++;
+                }
+                return s;
+            }
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
